61f458d0f5d0d7f1a3d3f91743cea30533c2bb88
itc-steve <sdane@infotrust.us>
2023-06-29 10:25:52 -0400

    new file:   FortiGate-CLI/GeoBlock.sh

---
+++ FortiGate-CLI/GeoBlock.sh
@@ -0,0 +1,436 @@
+# Copy Within Comments - Command 1 - Add Individual GeoBlock Regions
+config firewall address
+    edit "Geo_Afghanistan"
+        set type geography
+        set country "AF"
+    next
+    edit "Geo_Albania"
+        set type geography
+        set country "AL"
+    next
+    edit "Geo_Algeria"
+        set type geography
+        set country "DZ"
+    next
+    edit "Geo_Angola"
+        set type geography
+        set country "AO"
+    next
+    edit "Geo_Argentina"
+        set type geography
+        set country "AR"
+    next
+    edit "Geo_Armenia"
+        set type geography
+        set country "AM"
+    next
+    edit "Geo_Azerbaijan"
+        set type geography
+        set country "AZ"
+    next
+    edit "Geo_Bahrain"
+        set type geography
+        set country "BH"
+    next
+    edit "Geo_Bangladesh"
+        set type geography
+        set country "BD"
+    next
+    edit "Geo_Belarus"
+        set type geography
+        set country "BY"
+    next
+    edit "Geo_Belize"
+        set type geography
+        set country "BZ"
+    next
+    edit "Geo_Benin"
+        set type geography
+        set country "BJ"
+    next
+    edit "Geo_Bhutan"
+        set type geography
+        set country "BT"
+    next
+    edit "Geo_Bosnia"
+        set type geography
+        set country "BA"
+    next
+    edit "Geo_Botswana"
+        set type geography
+        set country "BW"
+    next
+    edit "Geo_Brunei"
+        set type geography
+        set country "BN"
+    next
+    edit "Geo_BurkinaFaso"
+        set type geography
+        set country "BF"
+    next
+    edit "Geo_Bulgaria"
+        set type geography
+        set country "BG"
+    next
+    edit "Geo_Burundi"
+        set type geography
+        set country "BI"
+    next
+    edit "Geo_Cambodia"
+        set type geography
+        set country "KH"
+    next
+    edit "Geo_Cameroon"
+        set type geography
+        set country "CM"
+    next
+    edit "Geo_CapeVerde"
+        set type geography
+        set country "CV"
+    next
+    edit "Geo_CentralAfricanRepublic"
+        set type geography
+        set country "CF"
+    next
+    edit "Geo_Chad"
+        set type geography
+        set country "TD"
+    next
+    edit "Geo_China"
+        set type geography
+        set country "CN"
+    next
+    edit "Geo_Colombia"
+        set type geography
+        set country "CO"
+    next
+    edit "Geo_Congo"
+        set type geography
+        set country "CG"
+    next
+    edit "Geo_CongoRepublic"
+        set type geography
+        set country "CD"
+    next
+    edit "Geo_Croatia"
+        set type geography
+        set country "HR"
+    next
+    edit "Geo_Cuba"
+        set type geography
+        set country "CU"
+    next
+    edit "Geo_Cyprus"
+        set type geography
+        set country "CY"
+    next
+    edit "Geo_Djibouti"
+        set type geography
+        set country "DJ"
+    next
+    edit "Geo_Ecuador"
+        set type geography
+        set country "EC"
+    next
+    edit "Geo_Egypt"
+        set type geography
+        set country "EG"
+    next
+    edit "Geo_ElSalvador"
+        set type geography
+        set country "SV"
+    next
+    edit "Geo_Eritrea"
+        set type geography
+        set country "ER"
+    next
+    edit "Geo_Estonia"
+        set type geography
+        set country "EE"
+    next
+    edit "Geo_Ethiopia"
+        set type geography
+        set country "ET"
+    next
+    edit "Geo_Gabon"
+        set type geography
+        set country "GA"
+    next
+    edit "Geo_Gambia"
+        set type geography
+        set country "GM"
+    next
+    edit "Geo_Georgia"
+        set type geography
+        set country "GE"
+    next
+    edit "Geo_Ghana"
+        set type geography
+        set country "GH"
+    next
+    edit "Geo_Guatemala"
+        set type geography
+        set country "GT"
+    next
+    edit "Geo_Guinea"
+        set type geography
+        set country "GN"
+    next
+    edit "Geo_Guyana"
+        set type geography
+        set country "GY"
+    next
+    edit "Geo_Haiti"
+        set type geography
+        set country "HT"
+    next
+    edit "Geo_Honduras"
+        set type geography
+        set country "HN"
+    next
+    edit "Geo_Indonesia"
+        set type geography
+        set country "ID"
+    next
+    edit "Geo_Iran"
+        set type geography
+        set country "IR"
+    next
+    edit "Geo_Iraq"
+        set type geography
+        set country "IQ"
+    next
+    edit "Geo_IvoryCoast"
+        set type geography
+        set country "CI"
+    next
+    edit "Geo_Jordan"
+        set type geography
+        set country "JO"
+    next
+    edit "Geo_Kazakhstan"
+        set type geography
+        set country "KZ"
+    next
+    edit "Geo_Kenya"
+        set type geography
+        set country "KE"
+    next
+    edit "Geo_Kuwait"
+        set type geography
+        set country "KW"
+    next
+    edit "Geo_Latvia"
+        set type geography
+        set country "LV"
+    next
+    edit "Geo_Lebanon"
+        set type geography
+        set country "LB"
+    next
+    edit "Geo_Liberia"
+        set type geography
+        set country "LR"
+    next
+    edit "Geo_Libya"
+        set type geography
+        set country "LY"
+    next
+    edit "Geo_Madagascar"
+        set type geography
+        set country "MG"
+    next
+    edit "Geo_Malawi"
+        set type geography
+        set country "MW"
+    next
+    edit "Geo_Mauritania"
+        set type geography
+        set country "MR"
+    next
+    edit "Geo_Morocco"
+        set type geography
+        set country "MA"
+    next
+    edit "Geo_Mozambique"
+        set type geography
+        set country "MZ"
+    next
+    edit "Geo_Namibia"
+        set type geography
+        set country "NA"
+    next
+    edit "Geo_Niger"
+        set type geography
+        set country "NE"
+    next
+    edit "Geo_Nigeria"
+        set type geography
+        set country "NG"
+    next
+    edit "Geo_NorthKorea"
+        set type geography
+        set country "KP"
+    next
+    edit "Geo_Oman"
+        set type geography
+        set country "OM"
+    next
+    edit "Geo_Pakistan"
+        set type geography
+        set country "PK"
+    next
+    edit "Geo_Palestine"
+        set type geography
+        set country "PS"
+    next
+    edit "Geo_Romania"
+        set type geography
+        set country "RO"
+    next
+    edit "Geo_Russia"
+        set type geography
+        set country "RU"
+    next
+    edit "Geo_CzechRep"
+        set type geography
+        set country "CZ"
+    next
+    edit "Geo_Kyrgyzstan"
+        set type geography
+        set country "KG"
+    next
+    edit "Geo_Laos"
+        set type geography
+        set country "LA"
+    next
+    edit "Geo_Lesotho"
+        set type geography
+        set country "LS"
+    next
+    edit "Geo_Lithuania"
+        set type geography
+        set country "LT"
+    next
+    edit "Geo_Malayasia"
+        set type geography
+        set country "MY"
+    next
+    edit "Geo_Moldova"
+        set type geography
+        set country "MD"
+    next
+    edit "Geo_Mongolia"
+        set type geography
+        set country "MN"
+    next
+    edit "Geo_Myanmar"
+        set type geography
+        set country "MM"
+    next
+    edit "Geo_Rwanda"
+        set type geography
+        set country "RW"
+    next
+    edit "Geo_Senegal"
+        set type geography
+        set country "SN"
+    next
+    edit "Geo_Sierra Leone"
+        set type geography
+        set country "SL"
+    next
+    edit "Geo_Slovakia"
+        set type geography
+        set country "SK"
+    next
+    edit "Geo_Somalia"
+        set type geography
+        set country "SO"
+    next
+    edit "Geo_SouthAfrica"
+        set type geography
+        set country "ZA"
+    next
+    edit "Geo_SouthSudan"
+        set type geography
+        set country "SS"
+    next
+    edit "Geo_Sudan"
+        set type geography
+        set country "SD"
+    next
+    edit "Geo_Swaziland"
+        set type geography
+        set country "SZ"
+    next
+    edit "Geo_Syria"
+        set type geography
+        set country "SY"
+    next
+    edit "Geo_Taiwan"
+        set type geography
+        set country "TW"
+    next
+    edit "Geo_Tajikistan"
+        set type geography
+        set country "TJ"
+    next
+    edit "Geo_Tanzania"
+        set type geography
+        set country "TZ"
+    next
+    edit "Geo_Thailand"
+        set type geography
+        set country "TH"
+    next
+    edit "Geo_Tunisia"
+        set type geography
+        set country "TN"
+    next
+    edit "Geo_Turkey"
+        set type geography
+        set country "TR"
+    next
+    edit "Geo_Turkmenistan"
+        set type geography
+        set country "TM"
+    next
+    edit "Geo_Uganda"
+        set type geography
+        set country "UG"
+    next
+    edit "Geo_Ukraine"
+        set type geography
+        set country "UA"
+    next
+    edit "Geo_UnitedArabEmirates"
+        set type geography
+        set country "AE"
+    next
+    edit "Geo_Uzbekistan"
+        set type geography
+        set country "UZ"
+    next
+    edit "Geo_Yemen"
+        set type geography
+        set country "YE"
+    next
+    edit "Geo_Zambia"
+        set type geography
+        set country "ZM"
+    next
+    edit "Geo_Zimbabwe"
+        set type geography
+        set country "ZW"
+    next
+end
+# End of Command 1
+
+# Copy Within Comments - Command 2 - Add GeoBlock Address Group
+config firewall addrgrp
+    edit "Group_GeoBlock"
+        set member "Geo_Afghanistan" "Geo_Albania" "Geo_Algeria" "Geo_Angola" "Geo_Argentina" "Geo_Armenia" "Geo_Azerbaijan" "Geo_Bahrain" "Geo_Bangladesh" "Geo_Belarus" "Geo_Belize" "Geo_Benin" "Geo_Bhutan" "Geo_Bosnia" "Geo_Botswana" "Geo_Brunei" "Geo_Bulgaria" "Geo_BurkinaFaso" "Geo_Burundi" "Geo_Cambodia" "Geo_Cameroon" "Geo_CapeVerde" "Geo_CentralAfricanRepublic" "Geo_Chad" "Geo_China" "Geo_Colombia" "Geo_Congo" "Geo_CongoRepublic" "Geo_Croatia" "Geo_Cuba" "Geo_Cyprus" "Geo_CzechRep" "Geo_Djibouti" "Geo_Ecuador" "Geo_Egypt" "Geo_ElSalvador" "Geo_Eritrea" "Geo_Estonia" "Geo_Ethiopia" "Geo_Gabon" "Geo_Gambia" "Geo_Georgia" "Geo_Ghana" "Geo_Guatemala" "Geo_Guinea" "Geo_Guyana" "Geo_Haiti" "Geo_Honduras" "Geo_Indonesia" "Geo_Iran" "Geo_Iraq" "Geo_IvoryCoast" "Geo_Jordan" "Geo_Kazakhstan" "Geo_Kenya" "Geo_Kuwait" "Geo_Kyrgyzstan" "Geo_Laos" "Geo_Latvia" "Geo_Lebanon" "Geo_Lesotho" "Geo_Liberia" "Geo_Libya" "Geo_Lithuania" "Geo_Madagascar" "Geo_Malawi" "Geo_Malayasia" "Geo_Mauritania" "Geo_Moldova" "Geo_Mongolia" "Geo_Morocco" "Geo_Mozambique" "Geo_Myanmar" "Geo_Namibia" "Geo_Niger" "Geo_Nigeria" "Geo_NorthKorea" "Geo_Oman" "Geo_Pakistan" "Geo_Palestine" "Geo_Romania" "Geo_Russia" "Geo_Rwanda" "Geo_Senegal" "Geo_Sierra Leone" "Geo_Slovakia" "Geo_Somalia" "Geo_SouthAfrica" "Geo_SouthSudan" "Geo_Sudan" "Geo_Swaziland" "Geo_Syria" "Geo_Taiwan" "Geo_Tajikistan" "Geo_Tanzania" "Geo_Thailand" "Geo_Tunisia" "Geo_Turkey" "Geo_Turkmenistan" "Geo_Uganda" "Geo_Ukraine" "Geo_UnitedArabEmirates" "Geo_Uzbekistan" "Geo_Yemen" "Geo_Zambia" "Geo_Zimbabwe"
+    next
+end
+# End of Command 2
